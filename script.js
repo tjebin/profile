7f4bb62ce94d109cc8a6bc86300bd2389e13e183
tjebin-github <shova_khan@yahoo.com>
2023-11-27 20:05:31 -0500

    Added Navigation; not finished

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+
+const container = document.querySelectorAll('.container');
+document.querySelector('.open-navbar-icon').
+    addEventListener('click', () => {
+        alert("clicked");
+        container.classList.add("change")
+    });
+// document.querySelector('.close-navbar-icon').
+//     addEventListener('click', () => {
+//         container.classList.remove("change")
+//     });
+
+const colors = ["#6495ed", "#7fffd4", "#ffa07a", "#f08080", "#afeeee"];
+let i = 0;
+Array.from(document.querySelectorAll(".nav-link")).
+    forEach((item) => {
+        item.style.cssText = `background-color: ${colors[i++]}`
+    });
+
 Array.from(document.querySelectorAll
     (".navigation-button")).forEach((item) => {
         item.onclick = () => {
