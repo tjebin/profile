c2eec2b80d4896ecfe178301d1e44845d49efba1
tjebin-github <shova_khan@yahoo.com>
2024-03-13 10:59:30 -0400

    Added new Faetures

--- script.js
+++ script.js
@@ -25,3 +25,13 @@ Array.from(document.querySelectorAll
             item.parentElement.parentElement.classList.toggle("change");
         }
     })
+
+const scrollline = document.querySelector('.scroll-line');
+function fillscrollline() {
+    const windowHeight = window.innerHeight;
+    const fullHeight = document.body.clientHeight;
+    const scrolled = window.scrollY;
+    const percentScrolled = (scrolled / (fullHeight - windowHeight)) * 100;
+    scrollline.style.width = percentScrolled + '%';
+};
+window.addEventListener('scroll', fillscrollline);
